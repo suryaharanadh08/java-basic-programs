4c6c53704e8ad9e82185cd1bc0ee498ad89cc4c6
suryaharanadh08 <badisuryaharanadh@gmail.com>
2024-08-06 20:16:15 +0530

    committed successfully

--- java basic 2 programs/demo.java
+++ java basic 2 programs/demo.java
@@ -1,26 +1,7 @@
 class demo
 {
     public static void main(String args[]){
-        int temp=2;
-        for(int i=1;i<=100;i++)
-        {
-            int count =0;
-            for(int j=1;j<=i;j++)
-            {
-                if(i%j==0)
-                {
-                    count++;
-                }
-            }
-            if(count==2)
-            {
-                if(temp%2==0)
-                {
-                    System.out.println(i);
-                   
-                }
-                 temp++;
-            }
-        }
+        String s="hello java"; 
+        System.out.println(s.contains("ol"));
     }
 }
