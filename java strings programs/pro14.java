c160f51af4ddf3a31c74d34a1a5ed7beb4769af7
suryaharanadh08 <badisuryaharanadh@gmail.com>
2024-08-08 19:04:50 +0530

    last commit for strings

--- java strings programs/pro14.java
+++ java strings programs/pro14.java
@@ -1,4 +1,19 @@
 class pro14
 {
+    public static void main(String[] args) {
+        String s = "welcome to java programming world";
+        String[] arr = s.split(" ");
+        String temp; 
+        String temp2;
+        System.out.println(arr[0]);
+        for(int i=0;i<arr.length-1;i++)
+        {
+            temp=arr[i];
+            char x=temp.charAt(temp.length()-1);
+            temp2= arr[i+1];
+            temp2=temp2.replace(temp2.charAt(0),x);
+            System.out.println(temp2);
+        }
+    }
     
 }
